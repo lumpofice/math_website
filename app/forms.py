3f69ac4abba4bcc87375c2ce03980102faa61f13
Jonathan Parker <jparker007@gmail.com>
2022-09-03 08:59:01 -0400

    learning how to interactively navigate the web app

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import FloatField, SubmitField
 from wtforms.validators import DataRequired
+    
 
-class GraphForm(FlaskForm):
+class ChooseFunction(FlaskForm):
+    submit = SubmitField('geometric')
+    
+    
+class GeometricForm(FlaskForm):
     base = FloatField('Base', validators=[DataRequired()])
     scalar = FloatField('Scalar', validators=[DataRequired()])
     submit = SubmitField('Show Graph')
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,24 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import GraphForm
+from app.forms import *
 from app.geometric_processing import Geometric
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', title='MassiveDiscipline')
+    geometric = ChooseFunction()
+    
+    if geometric.validate_on_submit():
+        
+        return redirect(url_for('geometric'))
+    
+    return render_template('index.html', title='MassiveDiscipline', form=geometric)
 
 
-@app.route('/graph', methods=['GET', 'POST'])
-def graph():
-    form = GraphForm()
+@app.route('/geometric', methods=['GET', 'POST'])
+def geometric():
+    form = GeometricForm()
     
     if form.validate_on_submit():
         
@@ -24,7 +30,7 @@ def graph():
         
         return redirect(url_for('graph_results'))
     
-    return render_template('graph.html', title='Graphs', form=form)
+    return render_template('geometric.html', title='Graphs', form=form)
 
 
 @app.route('/graph_results')
