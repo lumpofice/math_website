bb67a6ea368f37055e37627d53f79a087efff106
Jonathan Parker <jparker007@gmail.com>
2022-11-02 08:01:26 -0400

    bringing the logarithmic transform function into a viewpage

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,7 @@ ReciprocalDegree0ByDegree1Transform
 from app.square_root_transform import SquareRootTransform
 from app.cube_root_transform import CubeRootTransform
 from app.general_exponential_transform import GeneralExponentialTransform
+from app.general_logarithmic_transform import GeneralLogarithmicTransform
 import numpy as np
 
 
@@ -482,4 +483,68 @@ def general_exponential_transform():
 def general_exponential_transform_graph_results():
     return render_template(\
     'general_exponential_transform_graph_results.html',\
+    title='MassiveDiscipline')
+
+
+@app.route('/general_logarithmic_transform',\
+methods=['GET', 'POST'])
+def general_logarithmic_transform():
+    form = GeneralLogarithmicTransformForm()
+    
+    if form.validate_on_submit():
+        
+        if form.x_scalar.data == 0:
+            flash('Parent Function Only')
+            
+        elif form.y_scalar.data == 0:
+            flash('Parent Function Only')
+            
+        elif form.x_reflection.data == 0:
+            flash('Parent Function Only')
+        
+        else:
+            
+            if form.x_reflection.data > 0:
+                flash('B = {} ____ h = {} ____ b = {} ____ c = {} ____ '\
+                'a = {} ____ '\
+                'k = {} ____ '\
+                'domain = ({}, {}) ____ '\
+                'range = ({}, {}) ____ VA: x = {}'\
+                .format(form.base.data, form.horizontal_shift.data,\
+                form.x_scalar.data,\
+                form.x_reflection.data, form.y_scalar.data,\
+                form.vertical_shift.data, form.horizontal_shift.data, np.inf,\
+                -np.inf, np.inf, form.horizontal_shift.data))
+                
+            elif form.x_reflection.data < 0:
+                flash('B = {} ____ h = {} ____ b = {} ____ c = {} ____ '\
+                'a = {} ____ '\
+                'k = {} ____ '\
+                'domain = ({}, {}) ____ '\
+                'range = ({}, {}) ____ VA: x = {}'\
+                .format(form.base.data, form.horizontal_shift.data,\
+                form.x_scalar.data,\
+                form.x_reflection.data, form.y_scalar.data,\
+                form.vertical_shift.data, -np.inf, form.horizontal_shift.data,\
+                -np.inf, np.inf, form.horizontal_shift.data))
+    
+        general_logarithmic_transform = GeneralLogarithmicTransform()
+        general_logarithmic_transform.graph(\
+        form.base.data,\
+        form.horizontal_shift.data, form.x_scalar.data, form.x_reflection.data,\
+        form.y_scalar.data, form.vertical_shift.data)
+        
+        return redirect(url_for(\
+        'general_logarithmic_transform_graph_results'))
+    
+    return render_template(\
+    'general_logarithmic_transform.html',\
+    title='MassiveDiscipline',\
+    form=form)
+
+
+@app.route('/general_logarithmic_transform_graph_results')
+def general_logarithmic_transform_graph_results():
+    return render_template(\
+    'general_logarithmic_transform_graph_results.html',\
     title='MassiveDiscipline')
