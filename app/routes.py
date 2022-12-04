0100c0c4012caea7ecb31952e10a58dfb02aef65
Jonathan Parker <jparker007@gmail.com>
2022-12-04 08:36:17 -0500

    successfully added a registration form to the website, which stores user information input to the form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app
+from app import app, db
 from app.forms import *
 from app.geometric_series import GeometricSeries
 from app.polynomial_degree_1_transform import PolynomialDegree1Transform
@@ -37,6 +37,22 @@ def login():
         return redirect(next_page)
         return redirect(url_for('index'))
     return render_template('login.html', title='MassiveDiscipline', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='MassiveDiscipline',\
+        form=form)
 
 
 @app.route('/')
