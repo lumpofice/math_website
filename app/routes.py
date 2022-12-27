5ff3c9731548f47f1b2c632a32d567cbedc7ad23
Jonathan Parker <jparker007@gmail.com>
2022-12-27 17:52:31 -0500

    adding a user profile page and a corresponding view function

--- app/routes.py
+++ app/routes.py
@@ -179,6 +179,17 @@ in. Currently, posts are displayed on this page.'''
     return render_template('index.html', title='MassiveDiscipline', posts=posts)
 
 
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
 @app.route('/score')
 @login_required
 def score():
