95102ebdea1d2781917be2f4ca413f732b2e7855
Jonathan Parker <jparker007@gmail.com>
2023-01-17 15:09:55 -0500

    adding an email server that can accept error logs

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
+'''importing the configuration file for the application'''
+from config import Config
+
+'''importing Pythong libraries and packages'''
+import logging
+from logging.handlers import SMTPHandler
+
 '''importing flask methods, extensions, and libraries'''
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -19,3 +25,21 @@ login.login_view = 'login'
 '''After the application instance is created:
 importing objects, methods, and scripts from the application'''
 from app import routes, models, errors
+
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],\
+            toaddrs=app.config['ADMINS'],\
+            subject='Massive Discipline Failure',\
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
