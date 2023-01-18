a611695127d892a5cc557541e6ebedb1db83e465
Jonathan Parker <jparker007@gmail.com>
2023-01-18 14:17:10 -0500

    adding code that creates and maintains a log file for the application

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from config import Config
 
 '''importing Pythong libraries and packages'''
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 '''importing flask methods, extensions, and libraries'''
 from flask import Flask
@@ -39,7 +40,19 @@ if not app.debug:
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],\
             toaddrs=app.config['ADMINS'],\
-            subject='Massive Discipline Failure',\
+            subject='Math Website Failure',\
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+        
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/math_website.log',\
+        maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Math Website startup')
