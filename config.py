9a96a250d0b3cfe9d34f1ff3ce33947fbae2ebd8
Jonathan Parker <jparker007@gmail.com>
2023-01-20 16:07:40 -0500

    maintainence part 29

--- config.py
+++ config.py
@@ -2,6 +2,14 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    '''The SECRET_KEY variable below is used to protect our web forms from
+CSRF. The "SECRET_KEY" variable serving as argument within the get() function
+is set by the application maintainer in the environment, while the string value
+on the other side of the "or" statement is use in the absence of the
+"SECRET_KEY": <"SECRET_KEY" value> key:value ordered pair within the dictionary
+resulting from a call to os.environment.'''
+    
+    
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Eyop7n*n._;>"&6L'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
