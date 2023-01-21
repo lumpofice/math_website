569afed2af249b148aefb1bc19e1399f3575940a
Jonathan Parker <jparker007@gmail.com>
2023-01-21 13:56:27 -0500

    maintainence part 30

--- config.py
+++ config.py
@@ -3,11 +3,28 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     '''The SECRET_KEY variable below is used to protect our web forms from
-CSRF. The "SECRET_KEY" variable serving as argument within the get() function
-is set by the application maintainer in the environment, while the string value
-on the other side of the "or" statement is use in the absence of the
-"SECRET_KEY": <"SECRET_KEY" value> key:value ordered pair within the dictionary
-resulting from a call to os.environment.'''
+CSRF. The "SECRET_KEY" dictionary key serving as argument within the
+os.environ.get() call is set by the application maintainer in the
+environment, while the string value on the other side of the "or" statement is
+used in the absence of the "SECRET_KEY": <"SECRET_KEY" value> key:value ordered
+pair within the dictionary resulting from a call to os.environment.
+
+As of 01/20/23, there is no key:value pair set by the maintainer.
+
+import os
+import pprint
+env_var = os.environ
+pprint.pprint(dict(env_var), width = 1)
+
+Also, there is no "DATABASE_URL":<"DATABASE_URL" value> key:value ordered pair
+within dict(os.environ). Thus, we have a configured database, "app.db", located
+within the master directory of the application, stored in the "basedir"
+variable. None of "MAIL_SERVER", "MAIL_PORT", "MAIL_USE_TLS", "MAIL_USERNAME",
+nor "MAIL_PASSWORD" exist as keys in dict(os.environ).
+
+Finally, Flask-SQLAlchemy comes with an event notification system that uses
+more resources when not explicitly set to "False". Given that we do not need
+this event notification system, we have set this variable to "False".'''
     
     
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Eyop7n*n._;>"&6L'
