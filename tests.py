cc401a533787d83f250e295666d7cbd217406373
Jonathan Parker <jparker007@gmail.com>
2023-02-18 12:33:03 -0500

    maintainence part 38

--- tests.py
+++ tests.py
@@ -7,19 +7,34 @@ from app import app, db
 from app.models import User, Post
 
 class UserModelCase(unittest.TestCase):
+    '''We have placed a new key "DATABASE_URL" and value "sqlite://" pair into
+our dict(os.envirion) dictionary'''
+    
+    
     def setUp(self):
+        '''We create an app_context attribute to allow us to access our
+    current_app proxy'''
+        
+        
         self.app_context = app.app_context()
         self.app_context.push()
         db.create_all()
         
         
     def tearDown(self):
+        '''We pop the app_context to tear down our current_app proxy'''
+        
+        
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
         
         
     def test_password_hashing(self):
+        '''Here, we set up a user in our User model and test the
+    "check_password" method'''
+        
+        
         u = User(username='susan')
         u.set_password('cat')
         self.assertFalse(u.check_password('dog'))
@@ -27,6 +42,10 @@ class UserModelCase(unittest.TestCase):
         
         
     def test_avatar(self):
+        '''Here, we set up a user in our User model and test the avatar
+    method'''
+        
+        
         u = User(username='john', email='john@example.com')
         self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
             'd4c74594d841139328695756648b6bd6?d=identicon&s=128'))
