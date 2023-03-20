4f1fb20738d25e8ae921fed30a3e1fe96a9f2913
Jonathan Parker <jparker007@gmail.com>
2023-03-20 16:57:24 -0400

    adding Moment from the flask moment library so that we generate uniform datetime values and setup for coversions

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 '''flask-login extension, which keeps track of the user's logged-in state.'''
 from flask_login import LoginManager
@@ -25,6 +26,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 '''After the application instance is created:
