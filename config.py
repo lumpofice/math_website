fa22722ccfe8ed802be76be40d1a0bffe4b7c38d
Jonathan Parker <jparker007@gmail.com>
2023-06-19 12:43:58 -0400

    relocating the .env file

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '~/dotenv_files/math_website/.env'))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
